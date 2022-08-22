7676550ace38052cc3216352a0c92d92b3c2621e
Gurtss <dani.kitunen@gmail.com>
2022-08-22 14:53:41 +0300

    done

---
+++ teht_2.3.py
@@ -0,0 +1,8 @@
+kanta = float(input("Anna suorakulmion kanta:"))
+korkeus = float(input("Anna suorakulmion korkeus:"))
+
+ala = kanta * korkeus
+piiri = kanta * 2 + korkeus * 2
+
+print(f"Ala = {ala}")
+print(f"piiri = {piiri}")
---
+++ teht_2.4.py
@@ -0,0 +1,10 @@
+eka = int(input("Anna ensimmäinen luku: "))
+toka = int(input("Anna toinen luku: "))
+kolmas = int(input("Anna kolmas luku: "))
+summa = eka + toka + kolmas
+tulo = eka * toka * kolmas
+keskiarvo = summa / 3
+
+print(f"Lukujen summa on {summa}")
+print(f"Lukujen tulo on {tulo}")
+print(f"Lukujen keskiarvo on {keskiarvo}")
