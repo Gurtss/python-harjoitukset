ff811bb8534ccb4ed16d44e9f7fcbf366d1c6c70
Gurtss <dani.kitunen@gmail.com>
2022-08-25 13:49:54 +0300

    parannus

--- teht 2/teht_2.1.py
+++ teht 2/teht_2.1.py
@@ -1,2 +1,3 @@
 nimi = input("Anna nimesi: ")
 print("Terve, " + nimi + "!")
+# print(f"Terve, {name}!") toinen tapa tehdä
--- teht 2/teht_2.3.py
+++ teht 2/teht_2.3.py
@@ -4,5 +4,5 @@ korkeus = float(input("Anna suorakulmion korkeus:"))
 ala = kanta * korkeus
 piiri = kanta * 2 + korkeus * 2
 
-print(f"Ala = {ala}")
-print(f"piiri = {piiri}")
+print(f"Pinta-ala on {ala:6.2f}.")
+print(f"Piiri on{piiri:6.2f}.")
