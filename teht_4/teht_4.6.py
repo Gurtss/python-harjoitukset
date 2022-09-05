92acff5d863d6209d46440935411e3f9940c89dd
Gurtss <dani.kitunen@gmail.com>
2022-09-05 10:52:25 +0300

    tehty muutos

--- teht_4/teht_4.6.py
+++ teht_4/teht_4.6.py
@@ -1,18 +1,17 @@
-import numpy as np
+import random
 
 N = int(input("Anna pisteiden määrä: "))
-radius = 1
 n = 0
+i = 0
 
-xCoordinates = np.random.default_rng().uniform(-1, 1, (N,))
-yCoordinates = np.random.default_rng().uniform(-1, 1, (N,))
-
-for i in range(N):
-    x = xCoordinates[i]
-    y = yCoordinates[i]
-    # Check if the points are inside the circle or not
-    if x ** 2 + y ** 2 <= radius ** 2:
+while i <= N:
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    if x ** 2 + y ** 2 <= 1:
         n = n + 1
+    i = i + 1
 
 area = 4 * n / N
 print("Pi:n arvo: ", area)
+
+
