24b0b1dc908375781f3601338e43a868805a7fff
Gurtss <dani.kitunen@gmail.com>
2022-09-07 09:40:11 +0300

    done

--- teht_5/teht_5.3.py
+++ teht_5/teht_5.3.py
@@ -3,14 +3,19 @@
 # Esimerkiksi luku 13 on alkuluku, koska se voidaan jakaa vain luvuilla 1 ja 13 siten, että jako menee tasan.
 # Toisaalta esimerkiksi luku 21 ei ole alkuluku, koska se voidaan jakaa tasan myös luvulla 3 tai luvulla 7.
 
-number = int(input("Anna tasaluku: "))
-divide = 2
+whole_number = int(input("Anna kokonaisluku"))
+numbers_list = [9, 8, 7, 6, 5, 4, 3, 2]
+is_prime = False
 
-while number > divide:
-    if number % divide == 0:
-        print(f"{number} ei ole alkuluku")
-        break
-    divide += 1
+if whole_number / whole_number == 1 and whole_number / 1 == whole_number:
+    is_prime = True
+
+for number in numbers_list:
+   if whole_number % number == 0:
+       is_prime = False
+
+
+if is_prime == True:
+    print(f"Luku {whole_number} on alkuluku")
 else:
-    print(f"{number} on alkuluku")
-
+    print(f"Luku {whole_number} ei ole alkuluku")
