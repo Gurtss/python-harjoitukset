d8732474a564133e7c05a25348b94c7c06d90019
Gurtss <dani.kitunen@gmail.com>
2022-09-08 09:55:04 +0300

    done

--- teht_6/teht_6.6.py
+++ teht_6/teht_6.6.py
@@ -1,13 +1,30 @@
-import math
 
-def pizza_count(divider, prize):
-    radius = divider / 2
-    area = math.pi * radius ** 2
-    print(area)
+def area(divider):
+    import math
+    a = math.pi * divider * divider
+    return a
+def unit_prize(area_a, prize):
+    cost = area_a / prize
+    return cost
 
-pizzarange = 2
+prize_one = float(input("Anna pizzan 1 hinta: "))
 
-for pizza in range(pizzarange):
-    circle_divider = int(input("Anna halkaisijan arvo: "))
-    pizza_prize = int(input("Anna pizzan hinta: "))
-    pizza_count(circle_divider, pizza_prize)
+divider_2 = float(input("Anna pizzan 1 halkaisia: "))
+
+prize_two = float(input("Anna pizzan 2 hinta: "))
+
+divider_two = float(input("Anna pizzan 2 halkaisia: "))
+
+pizza_one_area = area(divider_2)
+
+pizza_two_area = area(divider_two)
+
+prize_one_area = unit_prize(pizza_one_area, prize_one)
+
+prize_two_area = unit_prize(pizza_two_area, prize_two)
+
+if prize_one_area > prize_two_area:
+    print ("Pizza 1 on parempi vastine rahalle.")
+
+else:
+    print ("Pizza 2 on parempi vastine rahalle.")
