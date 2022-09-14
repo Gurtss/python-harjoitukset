5e986fa9b08698393055933b8b0464986fdbf1e7
Gurtss <dani.kitunen@gmail.com>
2022-09-14 14:56:46 +0300

    parannus

--- teht_7/teht_7.1.py
+++ teht_7/teht_7.1.py
@@ -1,5 +1,8 @@
 
-seasons = ("talvi","talvi", "kevät","kevät","kevät", "kesä", "kesä", "kesä", "syksy", "syksy", "syksy", "talvi")
+seasons = ("talvi", "talvi", "kevät","kevät","kevät", "kesä", "kesä", "kesä", "syksy", "syksy", "syksy", "talvi")
 months = int(input("Anna kuukaden järjestysnumero (1-12): "))
 month = seasons[months-1]
-print (f"{months}. kuukausi on {month}.")
+if months <= 12 and months > 0:
+    print(f"{months}. kuukausi on {month}.")
+else:
+    print("Virhe")
--- teht_8/teht_8.3.py
+++ teht_8/teht_8.3.py
@@ -1,7 +1,6 @@
-#8.3
-import re
+
 import mysql.connector
-from geopy.distance import geodesic as distance
+from geopy import distance
 
 
 #yhteyden avaus
@@ -14,50 +13,21 @@ yhteys = mysql.connector.connect(
         autocommit=True
         )
 
-#funktio str muutokseen
-def convert(name):
-    new_name = ''.join(map(str, name))
-    return new_name
+def search(icao):
+    sql = "select latitude_deg, longitude_deg from airport where gps_code = '"+icao+"'"
+    cursor = yhteys.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    if cursor.rowcount > 0:
+        return result
+    else:
+        print("Kenttää ei löydy")
 
+locations = []
+for i in range(2):
+    icao = input(f"Anna {i+1}. ICAO koodi: ")
+    locations.append(search(icao))
+print(locations)
 
-#määritetään kysely
-icao_1 = input("Anna ensimmäisen lentokentän ICAO-koodi: ")
-icao_2 = input("Anna toisen lentokentän ICAO-koodi: ")
-sql_name_1 = "SELECT name FROM airport WHERE ident = '" + icao_1 + "'"
-sql_1 = "SELECT latitude_deg, longitude_deg FROM airport WHERE ident = '" + icao_1 + "'"
-sql_name_2 = "SELECT name FROM airport WHERE ident = '" + icao_2 + "'"
-sql_2 = "SELECT latitude_deg, longitude_deg FROM airport WHERE ident = '" + icao_2 + "'"
-
-#suoritetaan kysely 1 NIMI
-kursori = yhteys.cursor()
-kursori.execute(sql_name_1)
-
-#haetaan 1 NIMI
-result_1_name = kursori.fetchall()
-name_1 = convert(result_1_name)
-
-#suoritetaan kysely 1 kordinaatit
-kursori = yhteys.cursor()
-kursori.execute(sql_1)
-#haetaan 1 kordinaatit
-result_1 = kursori.fetchall()
-
-
-#suoritetaan kysely 2 NIMI
-kursori = yhteys.cursor()
-kursori.execute(sql_name_2)
-
-#haetaan 2 NIMI
-result_2_name = kursori.fetchall()
-#muuntaa lentoaseman nimen str muotoon ja poistaa ylimääräiset merkit
-name_2 = convert(result_2_name)
-
-#suoritetaan kysely 2 kordinaatit
-kursori = yhteys.cursor()
-kursori.execute(sql_2)
-#haetaan 2 kordinaatit
-result_2 = kursori.fetchall()
-
-
-#tulostaa vastauksen
-print(name_1,"ja", name_2,"etäisyys on: ", round(distance(result_1, result_2).km, 2), " km")
+dist = distance.distance(locations[0], locations[1]).km
+print(f"Etäisyys on {dist:.2f} km")
